4487c75e16959a781795ec8813452e2c1203e7b5
james-kidd <james.kidd02@hotmail.com>
2025-01-30 17:17:28 +0000

    current ver. not operational. MOVING TO TEST FOR CHAP 3

--- rango/urls.py
+++ rango/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path(
         "category/<slug:category_name_slug>/", views.show_category, name="show_category"
     ),
+    path(
+        "page/<slug:page_name_slug>/", views.show_page, name="show_page"
+    ),
 ]
